9be44434043e3d552217f6db431ddd6fe9fccb13
iaink <iainkinnish@googlemail.com>
2016-10-10 16:14:45 +0000

    Chpater 12: Completed

--- app.rb
+++ app.rb
@@ -1,7 +1,13 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat?' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
