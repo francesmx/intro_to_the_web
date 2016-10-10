0b850befadb859f9a60b90c22b18f4c7a4824b72
Frances Maxwell <fmaxwell@gmail.com>
2016-10-10 16:56:02 +0100

    Separated logic from view

--- app.rb
+++ app.rb
@@ -1,22 +1,7 @@
 require 'sinatra'
 set :session_secret, 'super secret'
 
-get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "I've got a stash of chocolate bars"
-end
-
-get '/frances' do
-  "I like KFC"
-end
-
-get '/Iain' do
-  "Iain lives in Southport"
-end
-
 get '/cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
   erb(:index)
 end
